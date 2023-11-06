992f246f15d4b2b38c54a0d2347261f3cf7bcf42
Pranto-Sen <prantosen56@gmail.com>
2023-11-06 23:45:34 +0600

    addFood api create

--- index.js
+++ index.js
@@ -26,17 +26,30 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
+    try {
+      const foodCollection = client
+        .db("foodShareDB")
+        .collection("foodCollection)");
+
+
+
+         app.post("/addFood", async (req, res) => {
+           const newFood = req.body;
+           console.log(newFood);
+           const result = await foodCollection.insertOne(newFood);
+           res.send(result);
+         });
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
