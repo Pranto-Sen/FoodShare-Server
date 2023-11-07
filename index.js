621533f74018b647811f5d758a54832b6117e0c7
Pranto-Sen <prantosen56@gmail.com>
2023-11-08 03:12:22 +0600

    create a api for get the requestfood data

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // console.log(process.env.DB_PASS);
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9jgoh4.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9jgoh4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,44 +25,108 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-      const foodCollection = client
-        .db("foodShareDB")
-        .collection("foodCollection)");
-      
-      const foodRequestCollection = client
-        .db("foodShareDB")
-        .collection("foodRequestCollection)");
+  try {
+    const foodCollection = client
+      .db("foodShareDB")
+      .collection("foodCollection)");
+
+    const foodRequestCollection = client
+      .db("foodShareDB")
+      .collection("foodRequestCollection)");
+
+    app.post("/addFood", async (req, res) => {
+      const newFood = req.body;
+      console.log(newFood);
+      const result = await foodCollection.insertOne(newFood);
+      res.send(result);
+    });
+
+    app.post("/requestFood", async (req, res) => {
+      const requestFood = req.body;
+      // console.log(req.body.foodname);
+          const filter = { _id: new ObjectId(req.body._id) }; // Use ObjectID to match the MongoDB _id field
+
+          const updateDocument = {
+            $set: {
+              "status": "Pending",
+            },
+          };
+
+      await foodCollection.updateOne(filter, updateDocument);
+      delete requestFood._id;
+      const result = await foodRequestCollection.insertOne(requestFood);
+      res.send(result);
+    });
+
+    // app.post("/requestFood", async (req, res) => {
+    //   const requestFood = req.body;
+
+    //   try {
+    //     const filter = { _id: new ObjectId(req.body._id) };
+    //     const updateDocument = {
+    //       $set: { status: "Pending" },
+    //     };
+
+    //     // Update the 'status' field in the 'foodCollection' collection
+    //     await foodCollection.updateOne(filter, updateDocument);
+
+    //     // Remove the '_id' field from the request body to avoid duplicate key error
+    //     delete requestFood._id;
+
+    //     // Insert the modified request body into 'foodRequestCollection'
+    //     const result = await foodRequestCollection.insertOne(requestFood);
+    //     res.send(result);
+    //   } catch (error) {
+    //     console.error("Error:", error);
+    //     res.status(500).send("An error occurred while processing the request.");
+    //   }
+    // });
+
+    app.get("/food", async (req, res) => {
+      const cursor = foodCollection.find({ status: 'Available' });
+      const result = await cursor.toArray();
+      result.sort((a, b) => b.foodquantity - a.foodquantity);
+      res.send(result);
+    });
+
+ 
+    app.get("/foodDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
+
+    //  app.get("/sortFood", async (req, res) => {
+    //     // const value = req.params.value; // Get email from the URL parameter
+    //     const cursor = await foodCollection.find(); // Assuming there's a field 'email' in your foodCollection
+    //     const result = await cursor.toArray();
+    //      result.sort((a, b) => b.expiredtime - a.expiredtime);
+    //    res.send(result);
+    //  });
 
 
+    //  app.get("/manageFood/", async (req, res) => {
+    //    const id = req.params.id;
+    //    const query = { _id: new ObjectId(id) };
+    //    const result = await foodCollection.findOne(query);
+    //    res.send(result);
+    //  });
 
-         app.post("/addFood", async (req, res) => {
-           const newFood = req.body;
-           console.log(newFood);
-           const result = await foodCollection.insertOne(newFood);
-           res.send(result);
-         });
-      
-        app.post("/requestFood", async (req, res) => {
-          const requestFood = req.body;
-          console.log(requestFood);
-          const result = await foodRequestCollection.insertOne(requestFood);
-          res.send(result);
-        });
-        
-         app.get("/food", async (req, res) => {
-           const cursor = foodCollection.find();
-             const result = await cursor.toArray();
-             result.sort((a, b) => b.foodquantity - a.foodquantity);
-           res.send(result);
-         });
-        
-         app.get("/food/:id", async (req, res) => {
-           const id = req.params.id;
-           const query = { _id: new ObjectId(id) };
-           const result = await foodCollection.findOne(query);
-           res.send(result);
-         });
+    // app.get("/manageFood/", async (req, res) => {
+    //   const userEmail = req.user.email; // Assuming you have the user's email from authentication
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id), email: userEmail };
+    //   const result = await foodCollection.findOne(query);
+
+    //   if (result) {
+    //     res.send(result);
+    //   } else {
+    //     res
+    //       .status(404)
+    //       .send("Food not found for the user's email and provided ID.");
+    //   }
+    // });
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
